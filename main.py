857d40d5b8c3ca003bff191ded0252ad61c11277
Marcel Parpart <subbyberlin@googlemail.com>
2018-06-13 03:44:22 +0200

    switch zu discord.py@rewrite

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python4
+"""
+# install discord.py/rewrite
+$ git clone https://github.com/Rapptz/discord.py.git -b rewrite
+
+# check with
+$ cd discord.py
+$ git branche
+
+# install
+$ python3 -m pip install -U discord.py
+"""
+import discord
+from discord.ext import commands
+import time
+import config
+
+description = None
+bot = commands.Bot(command_prefix='?')
+
+@bot.event
+async def on_ready():
+    print('Logged in as: ' + bot.user.name)
+    print('user-ID: ' + bot.user.id)
+    print('---------------------------')
+    await bot.change_presence(game=discord.Game(name='Pokémon Go'))
+
+
 '''
 Problemstellung: Teamwahl
 - Teams: Instinct, Mystic, Valor
@@ -7,50 +34,44 @@ Problemstellung: Teamwahl
             andere.
           - ein späterer wechsel soll nicht ermöglicht werden (außer
             durch Admins)
-          - 
+          -
 '''
-import discord
-import asyncio
+#@bot.command()
+#async def team();
+'''
+Feldforschung: Usern ermöglicen über den bot quests in dem dafür vorgesehenen
+channel zu posten/bearbeiten. tägleich 0:00 soll der channel bis auf die
+angepinnten nachrichten gelehrt werden.
+'''
+@bot.command()
+async def research():
+    print("research send embed.")
+    embed = discord.Embed(
+        #title = "Titel",
+        colour = discord.Colour.dark_orange(),
+        description = "**Auftrag:** Fange 10 Pokémon.\n"
+                    "**Belohnung:** Pokémon _(Karpador)_\n"
+                    "**Pokéstop:** Augen\n"
+                    "**Ort:** Borkheider Str. 39 (Marzahn)\n"
+                    "**Maps:** https://goo.gl/maps/oohDawDXUF92"
+    )
+    embed.set_thumbnail(url="https://files.pokefans.net/images/pokemon-go/modelle/129.png")
+    embed.set_author(
+        name="Feldforschung",
+        url="https://pokefans.net/spiele/pokemon-go/liste-aller-feldforschungen",
+        icon_url="http://files.pokefans.net/images/pokemon-go/findenicons/forschung.png"
+    )
+    #embed.set_image(url="http://files.pokefans.net/images/pokemon-go/findenicons/forschung.png")
+    #embed.set_footer(text="das ist der Footer")
+    #await bot.delete_message(message)
+    await bot.say(embed=embed)
 
-import config
+@bot.command()
+async def ping():
+    t1 = time.perf_counter()
+    msg = await bot.say(":zap: **calculating**")
+    t2 = time.perf_counter()
+    speed = t2 - t1
+    await bot.edit_message(msg, f":zap: **{round(speed * 1000)}**ms")
 
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print('Logged in as: ' + client.user.name)
-    print('user-ID: ' + client.user.id)
-    print('----------------------------------')
-    await client.change_presence(game=discord.Game(name='Pokémon Go'))
-
-def check_role_availability(role_name, roles):
-    """
-        Testet ob role_name als Rolle (role.name) in dem Array roles enthalten ist.
-
-        Returns: True, wenn Rolle vorhanden
-        Returns: False, wenn Rolle nicht vorhanden
-    """
-    for role in roles:
-        if role.name == role_name:
-            return True
-
-    return False
-
-@client.event
-async def on_message(message):
-    if message.content.lower() == '.instinct':
-        role_instinct = discord.utils.get(message.server.roles, name="Instinct")
-        await client.add_roles(message.author, role_instinct)
-        await client.send_message(message.channel, 'funzt?')
-
-    if message.content.lower() == '.mystic':
-        role_name = "Mystic"
-        if check_role_availability(role_name, message.author.roles):
-            print("Mystic ist bereits in der Rolle {} ({})".format(role_name, message.author.roles))
-            await client.send_message(message.channel, "Du bist bereits im Team Mystic")
-        else:
-            print("Fuege dich der Rolle {} hinzu.".format(role_name))
-            role = discord.utils.get(message.server.roles, name=role_name)
-            await client.add_roles(message.author, role)
-
-client.run(config.TOKEN_DISCORD_BOT)
+bot.run(config.TOKEN_DISCORD_BOT)
